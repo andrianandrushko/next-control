8cec6061b75baea6d92f86d241f8aadc06457fd6
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-08-10 19:57:59 +0300

    commit

---
+++ app/components/UserInfo.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+type Props = {
+    avatar: string;
+    name: string;
+};
+const UserInfo = ({ avatar}: Props) => {
+    return (
+        <div className="user-info">
+            <Image src={avatar} alt="avatar" width={32} height={32} className="avatar" />
+            <span>Andrian</span>
+        </div>
+    );
+};
+
+export default UserInfo;
