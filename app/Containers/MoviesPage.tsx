88849c09bd8f6f8ecab83fd9840031ea5c5c8f36
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-08-11 12:17:41 +0300

    commit

--- app/Containers/MoviesPage.tsx
+++ app/Containers/MoviesPage.tsx
@@ -1,17 +1,11 @@
 import React from 'react';
-import {getMovies} from "@/app/services/api.service";
-import type {IMovie} from "@/app/models/IMovie";
-const MoviesPage = async () => {
-        const { results: movies } = await getMovies();
-        return (
-            <div>
-                {movies.map((movie: IMovie) => (
-                    <div key={movie.id}>
-                        <p>{movie.id}</p>
-                        <p>{movie.title}</p>
-                    </div>
-                ))}
-            </div>
-        );
-    };
+
+const MoviesPage = () => {
+    return (
+        <div>
+
+        </div>
+    );
+};
+
 export default MoviesPage;
---
+++ app/components/MovieInfo.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import {IMovie} from "@/app/models/IMovie";
+import Image from "next/image";
+type Props = {
+    movie:IMovie;
+}
+const MovieInfo = async ({movie}:Props) => {
+    return (
+        <div>
+                <div key={movie.id}>
+                    <p>{movie.title}</p>
+                    <p className={'overview'}><Image src='/description.png' alt="description" className={'description'}/> : {movie.overview}</p>
+                </div>
+        </div>
+    );
+};
+
+export default MovieInfo;
