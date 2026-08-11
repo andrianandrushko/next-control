59be94deb41c1f2459f4b5b022e10abaa2d109dc
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-08-11 19:50:58 +0300

    commit

--- app/Paginations/pagination.tsx
+++ app/Paginations/pagination.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
 import Link from "next/link";
 
 type Props = {
     pg: string;
-}
+};
+
 const Pagination = ({ pg }: Props) => {
     const currentPage = Number(pg) || 1;
-    return (
-        <div className='next-prev'>
-             <button className='next'>
-                 <Link
-                     href={`?pg=${currentPage + 1}`}>
-                 </Link>
-                 next</button>
 
-            {currentPage > 1 &&
-              <button className='prev'>
-                  <Link
-                      href={`?pg=${currentPage -1}`}
-                  ></Link>
-                  prev</button>
-            }
+    return (
+        <div className="next-prev">
+            <Link
+                className="next"
+                href={`?pg=${currentPage + 1}`}
+            >
+                next
+            </Link>
+
+            <Link
+                className="prev"
+                href={`?pg=${Math.max(1, currentPage - 1)}`}
+            >
+                prev
+            </Link>
         </div>
     );
 };
