6f1170acd65c3ebcea12ff56b4cfe15af7848b3e
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-08-13 13:28:46 +0300

    commit

--- app/components/UserInfo.tsx
+++ app/components/UserInfo.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 const UserInfo = () => {
     return (
         <div className="user-info">
-            <Image src='/avatar.gif' alt="avatar" width={32} height={32} className='user-info' unoptimized />
+            <Image src='/avatar.gif' alt="avatar" width={32} height={32} className='user-info'/>
             <span>Andrian</span>
         </div>
     );
