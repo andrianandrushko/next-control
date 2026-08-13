20b939eec679c0017e709c29dfd9de08e9c8effb
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-08-13 20:03:39 +0300

    commit

--- src/components/PosterPreview/PosterPreview.tsx
+++ src/components/PosterPreview/PosterPreview.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image';
 
 type Props = {
-    poster_path: string,
+    poster_path: string | null,
 }
 
 const PosterPreview = ({ poster_path }: Props) => {
@@ -10,7 +10,7 @@ const PosterPreview = ({ poster_path }: Props) => {
     }
         return (
             <div>
-                <Image src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt="Movie poster" width={600} height={700}
+                <Image src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt="Movie poster" width={400} height={300}
                        unoptimized/>
             </div>
         );
