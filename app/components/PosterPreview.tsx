4c9b70d91404338c7061911aa981fd7bcade66b0
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-08-13 14:00:56 +0300

    commit

--- app/components/PosterPreview.tsx
+++ app/components/PosterPreview.tsx
@@ -6,13 +6,14 @@ type Props = {
 
 const PosterPreview = ({ poster_path }: Props) => {
     if (!poster_path) {
-        return null;
+        return (
+            <div>
+                <Image src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt="Movie poster" width={600} height={700}
+                       unoptimized/>
+            </div>
+        );
     }
-    return (
-        <div>
-            <Image  src={`https://image.tmdb.org/t/p/w500${poster_path}`} alt="Movie poster" width={600} height={700} unoptimized/>
-        </div>
-    );
-};
+
+}
 
 export default PosterPreview;
