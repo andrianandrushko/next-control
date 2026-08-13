cc4caec436f8a5ddf8110d7c462a057e6b65a979
andrianandrushkoчXXX <andrushkoandrian@gmail.com>
2026-08-13 13:23:56 +0300

    commit

--- app/components/MovieInfo.tsx
+++ app/components/MovieInfo.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
 import {IMovie} from "@/app/models/IMovie";
 import Image from "next/image";
 type Props = {
     movie:IMovie;
 }
-const MovieInfo = async ({movie}:Props) => {
+const MovieInfo = ({movie}:Props) => {
     return (
         <div className="MovieInfo">
-                <div key={movie.id}>
+                <div>
                     <p className='title'>{movie.title}</p>
-                    <p className='overview'><Image src='/description.gif' alt="description" width={70} height={5} className={'description'}/> : {movie.overview}</p>
+                    <p className='overview'><Image src='/description.gif' alt="description" width={20} height={20} className={'description'}/> : {movie.overview}</p>
                 </div>
         </div>
     );
